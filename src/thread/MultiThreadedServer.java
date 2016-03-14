b79df4bc95d734cb1991474c224e95bcf63892e6
TimothyPaul Shaker <timothy.shaker@gmail.com>
2016-03-14 14:52:13 -0400

    Establish connection

--- src/thread/MultiThreadedServer.java
+++ src/thread/MultiThreadedServer.java
@@ -1,5 +1,6 @@
 package thread;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,8 +11,17 @@ public class MultiThreadedServer {
 		    // create a server side socket - same as before
 		    ServerSocket server = new ServerSocket(9999);
 		    while(true) {
-		        Socket socket = server.accept();
-		        new ThreadedSocket(socket).start();
+		    	try {
+			        Socket socket = server.accept();
+			        new ThreadedSocket(socket).start();
+		    	} catch(IOException e) {
+		    		try {
+						server.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+		    	}
 		    }
-	  } 
+	  }
 } 
--- src/thread/ThreadedSocket.java
+++ src/thread/ThreadedSocket.java
@@ -1,5 +1,9 @@
 package thread;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ThreadedSocket extends Thread {
@@ -10,6 +14,37 @@ public class ThreadedSocket extends Thread {
 	}
 	
 	public void run() {
+		BufferedReader input = null;
+		PrintWriter output = null;
 		
+		try {
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.exit(-1);
+		}
+		
+		while(true) {
+			try {
+				output.println(input.readLine());
+				System.out.println(input.readLine());
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.exit(-1);
+			}
+		}
 	}
 }
